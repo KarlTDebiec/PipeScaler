4c32a6c20246864879389566c2937f864f4ee78a
Karl Debiec <karl.t.debiec@gmail.com>
2020-03-06 08:22:19 -0600

    Fixed WaifuProcessor.py for images smaller than 200x200

--- lauhseuisin/processors/WaifuProcessor.py
+++ lauhseuisin/processors/WaifuProcessor.py
@@ -10,9 +10,13 @@
 ################################### MODULES ###################################
 from __future__ import annotations
 
+from os import remove
 from os.path import isfile
 from subprocess import Popen
-from typing import Any, List
+from tempfile import NamedTemporaryFile
+from typing import Any, List, Optional, IO
+
+from PIL import Image
 
 from lauhseuisin.processors import Processor
 
@@ -31,15 +35,35 @@ class WaifuProcessor(Processor):
     def process_file(self, infile: str, outfile: str) -> None:
         if isfile(outfile):
             return
+        image = Image.open(infile)
+        original_size = image.size
+        tempfile: Optional[IO[bytes]] = None
+        if original_size[0] < 200 or original_size[1] < 200:
+            tempfile = NamedTemporaryFile(delete=False, suffix=".png")
+            expanded_image = Image.new(
+                image.mode, (max(200, original_size[0]),
+                             max(200, original_size[1])))
+            expanded_image.paste(
+                image, (0, 0, original_size[0], original_size[1]))
+            expanded_image.save(tempfile)
+            tempfile.close()
+            intermediate_infile = tempfile.name
+        else:
+            intermediate_infile = infile
         print(f"Processing to '{outfile}'")
         command = f"{self.executable} " \
                   f"-t {self.imagetype} " \
                   f"-s {self.scale} " \
                   f"-n {self.noise} " \
-                  f"-i {infile} " \
+                  f"-i {intermediate_infile} " \
                   f"-o {outfile}"
         print(command)
         Popen(command, shell=True, close_fds=True).wait()
+        if tempfile is not None:
+            Image.open(outfile).crop(
+                (0, 0, original_size[0] * self.scale,
+                 original_size[1] * self.scale)).save(outfile)
+            remove(tempfile.name)
 
     @classmethod
     def get_pipes(cls, **kwargs: Any) -> List[Processor]:
