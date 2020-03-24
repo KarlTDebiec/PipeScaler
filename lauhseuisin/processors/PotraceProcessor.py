bd3285fb605cb6a8756d79606faa213984b89715
Karl Debiec <karl.t.debiec@gmail.com>
2020-03-23 20:10:18 -0500

    Made PotraceProcessor.py a command-line tool

--- lauhseuisin/processors/PotraceProcessor.py
+++ lauhseuisin/processors/PotraceProcessor.py
@@ -10,8 +10,9 @@
 ################################### MODULES ###################################
 from __future__ import annotations
 
-from os import remove
-from os.path import expandvars, splitext
+from argparse import ArgumentParser, ArgumentError
+from os import remove, access, R_OK
+from os.path import expandvars, splitext, isfile
 from subprocess import Popen
 from typing import Any
 
@@ -21,51 +22,120 @@ from lauhseuisin.processors.Processor import Processor
 ################################### CLASSES ###################################
 class PotraceProcessor(Processor):
 
-    def __init__(self, blacklevel: float = 0.3, alphamax: float = 1.34,
-                 opttolerance: float = 0.2,
-                 convert_executable: str = "convert",
-                 potrace_executable: str = "potrace", **kwargs: Any) -> None:
+    def __init__(self, convert_executable: str = "convert",
+                 potrace_executable: str = "potrace", blacklevel: float = 0.3,
+                 alphamax: float = 1.34, opttolerance: float = 0.2,
+                 **kwargs: Any) -> None:
         super().__init__(**kwargs)
 
+        self.convert_executable = expandvars(convert_executable)
+        self.potrace_executable = expandvars(potrace_executable)
         self.blacklevel = blacklevel
         self.alphamax = alphamax
         self.opttolerance = opttolerance
-        self.convert_executable = expandvars(convert_executable)
-        self.potrace_executable = expandvars(potrace_executable)
+
         self.desc = f"potrace-{self.blacklevel:4.2f}-" \
                     f"{self.alphamax:4.2f}-{self.opttolerance:3.1f}"
 
     def process_file_in_pipeline(self, infile: str, outfile: str) -> None:
+        self.process_file(infile, outfile, self.convert_executable,
+                          self.potrace_executable, self.blacklevel,
+                          self.alphamax, self.opttolerance,
+                          self.pipeline.verbosity)
+
+    # region Public Class Methods
+    @classmethod
+    def construct_argparser(cls) -> ArgumentParser:
+        """
+        Constructs argument parser
+
+        Returns:
+            parser (ArgumentParser): Argument parser
+        """
+
+        def infile_argument(value: str) -> str:
+            if not isinstance(value, str):
+                raise ArgumentError()
+
+            value = expandvars(value)
+            if not isfile(value):
+                raise ArgumentError(f"infile '{value}' does not exist")
+            elif not access(value, R_OK):
+                raise ArgumentError(f"infile '{value}' cannot be read")
+
+            return value
+
+        parser = super().construct_argparser(description=__doc__)
+
+        parser.add_argument(
+            "-c", "--convert_executable",
+            default="convert",
+            type=str,
+            help="path to ImageMagick convert executable")
+        parser.add_argument(
+            "-p", "--potrace_executable",
+            default="potrace",
+            type=str,
+            help="path to potrace executable")
+        parser.add_argument(
+            "-k", "--blacklevel",
+            default=0.3,
+            type=float,
+            help="black/white cutoff in input file (default: 0.3)")
+        parser.add_argument(
+            "-a", "--alphamax",
+            default=1.34,
+            type=float,
+            help="corner threshold parameter (default: 1.34)")
+        parser.add_argument(
+            "-O", "----opttolerance",
+            default=0.2,
+            type=float,
+            help="curve optimization tolerance (default: 0.2)")
+
+        return parser
+
+    @classmethod
+    def process_file(cls, infile: str, outfile: str, convert_executable: str,
+                     potrace_executable: str, blacklevel: float,
+                     alphamax: float, opttolerance: float, verbosity: int):
+
         # Convert to bmp; potrace does not accept png
         bmpfile = f"{splitext(infile)[0]}.bmp"
-        command = f"{self.convert_executable} " \
+        command = f"{convert_executable} " \
                   f"{infile} " \
                   f"{bmpfile}"
-        if self.pipeline.verbosity >= 1:
+        if verbosity >= 1:
             print(command)
         Popen(command, shell=True, close_fds=True).wait()
 
         # trace
         svgfile = f"{splitext(outfile)[0]}.svg"
-        command = f"{self.potrace_executable} " \
+        command = f"{potrace_executable} " \
                   f"{bmpfile} " \
                   f"-b svg " \
-                  f"-k {self.blacklevel} " \
-                  f"-a {self.alphamax} " \
-                  f"-O {self.opttolerance} " \
+                  f"-k {blacklevel} " \
+                  f"-a {alphamax} " \
+                  f"-O {opttolerance} " \
                   f"-o {svgfile}"
-        if self.pipeline.verbosity >= 1:
+        if verbosity >= 1:
             print(command)
         Popen(command, shell=True, close_fds=True).wait()
 
         # Rasterize svg to png
-        command = f"{self.convert_executable} " \
+        command = f"{convert_executable} " \
                   f"{svgfile} " \
                   f"{outfile}"
-        if self.pipeline.verbosity >= 1:
+        if verbosity >= 1:
             print(command)
         Popen(command, shell=True, close_fds=True).wait()
 
         # Clean up
         remove(bmpfile)
         remove(svgfile)
+    # endregion
+
+
+#################################### MAIN #####################################
+if __name__ == "__main__":
+    PotraceProcessor.main()
