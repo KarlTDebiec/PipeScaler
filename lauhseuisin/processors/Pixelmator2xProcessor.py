Fixed scaling transparency bugs

lauhseuisin/processors/Pixelmator2xProcessor.py
@@ -110,7 +110,7 @@ class Pixelmator2xProcessor(Processor):
             merged_data[:, :, 3] = np.array(rgba_image)[:, :, 3]
             rgba_image = Image.fromarray(merged_data)
 
-        # Scale to 2X
+        # Save image
         rgba_image.save(outfile)
 
 

lauhseuisin/processors/ResizeProcessor.py
@@ -33,11 +33,22 @@ class ResizeProcessor(Processor):
     @classmethod
     def process_file(cls, infile: str, outfile: str, scale: float,
                      verbosity: int) -> None:
+        # Load image
         input_image = Image.open(infile)
+        size = (int(np.round(input_image.size[0] * scale)),
+                int(np.round(input_image.size[1] * scale)))
 
-        output_image = input_image.resize((
-            int(np.round(input_image.size[0] * scale)),
-            int(np.round(input_image.size[1] * scale))),
-            resample=Image.LANCZOS)
+        # Scale image in both RGB and RGBA modes
+        rgb_image = input_image.convert("RGB").resize(
+            size, resample=Image.LANCZOS)
+        rgba_image = input_image.resize(
+            size, resample=Image.LANCZOS)
 
+        # Combine R, G, and B from RGB with A from RGBA
+        merged_data = np.zeros((size[1], size[0], 4), np.uint8)
+        merged_data[:, :, :3] = np.array(rgb_image)
+        merged_data[:, :, 3] = np.array(rgba_image)[:, :, 3]
+        output_image = Image.fromarray(merged_data)
+
+        # Save image
         output_image.save(outfile)

lauhseuisin/sorters/MipmapSorter.py
@@ -91,14 +91,29 @@ class MipmapSorter(Sorter):
                                 print(f"{self}: {outfile}")
 
                             if not isfile(outfile):
+                                # Load and scale image
                                 input_image = Image.open(infile)
-                                output_image = input_image.resize((
-                                    int(np.round(
-                                        input_image.size[0] * scale)),
-                                    int(np.round(
-                                        input_image.size[1] * scale))),
-                                    resample=Image.LANCZOS)
+                                size = (
+                                    int(np.round(input_image.size[0] * scale)),
+                                    int(np.round(input_image.size[1] * scale)))
+                                output_image = input_image.convert(
+                                    "RGB").resize(
+                                    size, resample=Image.LANCZOS)
+
+                                if input_image.mode == "RGBA":
+                                    rgba_image = input_image.resize(
+                                        size, resample=Image.LANCZOS)
+                                    merged_data = np.zeros(
+                                        (size[1], size[0], 4), np.uint8)
+                                    merged_data[:, :, :3] = np.array(
+                                        output_image)
+                                    merged_data[:, :, 3] = np.array(
+                                        rgba_image)[:, :, 3]
+                                    output_image = Image.fromarray(merged_data)
+
+                                # Save image
                                 output_image.save(outfile)
+
                             self.log_outfile(outfile)
                             if self.downstream_pipes is not None:
                                 for pipe in self.downstream_pipes: