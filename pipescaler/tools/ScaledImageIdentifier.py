91522d64f6947711721054cc8057c55c2ea5ac90
Karl Debiec <karl.t.debiec@gmail.com>
2020-08-09 17:42:12 -0500

    ScaledImageIdentifier.py WIP

--- pipescaler/tools/ScaledImageIdentifier.py
+++ pipescaler/tools/ScaledImageIdentifier.py
@@ -15,13 +15,8 @@ from os.path import splitext
 from typing import Dict, Tuple
 
 import numpy as np
-import yaml
-from IPython import embed
 from PIL import Image
 from skimage.metrics import structural_similarity as ssim
-
-if False:
-    embed()
 
 
 ################################### CLASSES ###################################
@@ -39,35 +34,41 @@ class ScaledImageIdentifier:
         # Store output directory for concatenations
 
     def __call__(self):
-        conf = f"{getenv('HOME')}/OneDrive/code/Zelda3DHD/OOT"
-        local = f"{getenv('HOME')}/Documents/OOT"
+        # conf = f"{getenv('HOME')}/OneDrive/code/Zelda3DHD/OOT"
+        # local = f"{getenv('HOME')}/Documents/OOT"
+        conf = f"{getenv('HOME')}/OneDrive/code/JSRHD/JSR"
+        local = f"{getenv('HOME')}/Documents/JSR"
 
-        skip = set(listdir(f"{local}/1x_skip"))
-        skip = skip.union(listdir(f"{local}/1x_interface"))
-        skip = skip.union(listdir(f"{local}/1x_large_text"))
-        skip = skip.union(listdir(f"{local}/1x_maps"))
-        skip = skip.union(listdir(f"{local}/1x_normal"))
+        skip = set(
+            [splitext(f)[0] for f in listdir(f"{local}/1x_skip")])
+        skip = skip.union(
+            [splitext(f)[0] for f in listdir(f"{local}/1x_interface")])
+        # skip = skip.union(
+        #     [splitext(f)[0] for f in listdir(f"{local}/1x_large_text")])
+        # skip = skip.union(
+        #     [splitext(f)[0] for f in listdir(f"{local}/1x_maps")])
+        # skip = skip.union(
+        #     [splitext(f)[0] for f in listdir(f"{local}/1x_normal")])
 
         # Load assigned images
-        with open(f"{conf}/scaled.yaml", "r") as f:
-            assignments = yaml.load(f, Loader=yaml.SafeLoader)
-        large_to_small = {}
-        small_to_large = {}
-        for large, scaled in assignments.items():
-            large_to_small[large] = []
-            for scale, smalls in scaled.items():
-                if not isinstance(smalls, list):
-                    smalls = [smalls]
-                for small in smalls:
-                    small_to_large[small] = (large, scale)
-                    large_to_small[large].append((small, scale))
-
-        skip = skip.union(small_to_large.keys())
+        # with open(f"{conf}/scaled.yaml", "r") as f:
+        #     assignments = yaml.load(f, Loader=yaml.SafeLoader)
+        # large_to_small = {}
+        # small_to_large = {}
+        # for large, scaled in assignments.items():
+        #     large_to_small[large] = []
+        #     for scale, smalls in scaled.items():
+        #         if not isinstance(smalls, list):
+        #             smalls = [smalls]
+        #         for small in smalls:
+        #             small_to_large[small] = (large, scale)
+        #             large_to_small[large].append((small, scale))
+        # skip = skip.union(small_to_large.keys())
 
         # Construct dictionary of sizes to dictionaries of images
         data: Dict[Tuple[int, int], Dict[str, np.ndarray]] = {}
         for infile in listdir(f"{local}/1x"):
-            if infile == ".DS_Store" or infile in skip:
+            if infile == ".DS_Store" or splitext(infile)[0] in skip:
                 continue
             image = Image.open(f"{local}/1X/{infile}")
             size = image.size
@@ -78,9 +79,15 @@ class ScaledImageIdentifier:
         # Loop over sizes from largest to smallest
         for size in reversed(sorted(data.keys())):
             print(f"{size}: {len(data[size])}")
+            for scale in [0.5, 0.25, 0.125, 0.0625]:
+                scaled_size = tuple(
+                    [int(size[0] * scale), int(size[1] * scale)])
+                if scaled_size in data:
+                    print(f"    {scaled_size}: {len(data[scaled_size])}")
 
             # Loop over images at this size
             for large_infile, large_datum in data[size].items():
+                # print(large_infile)
 
                 # Loop over scales from largest to smallest
                 for scale in [0.5, 0.25, 0.125, 0.0625]:
@@ -88,7 +95,7 @@ class ScaledImageIdentifier:
                         [int(size[0] * scale), int(size[1] * scale)])
                     if scaled_size not in data:
                         continue
-                    print(f"    {scaled_size}: {len(data[scaled_size])}")
+                    # print(f"    {scaled_size}: {len(data[scaled_size])}")
                     scaled_datum = np.array(
                         Image.fromarray(large_datum).resize(
                             scaled_size, resample=Image.LANCZOS))
@@ -101,9 +108,9 @@ class ScaledImageIdentifier:
                         score = ssim(scaled_datum, small_datum,
                                      multichannel=True)
 
-                        print(f"        {large_infile} {small_infile} "
-                              f"{score:4.2f}")
                         if score > 0.7:
+                            print(f"        {large_infile} {small_infile} "
+                                  f"{score:4.2f}")
                             Image.fromarray(large_datum).show()
                             Image.fromarray(small_datum).show()
 
