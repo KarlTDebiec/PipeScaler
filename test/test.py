d920f235f46a763284b289a99881dd91012cfcda
Karl Debiec <karl.t.debiec@gmail.com>
2020-08-17 21:11:40 -0500

    tests wip

--- test/test.py
+++ test/test.py
@@ -7,17 +7,24 @@
 #   This software may be modified and distributed under the terms of the
 #   BSD license. See the LICENSE file for details.
 ################################### MODULES ###################################
+
+import pytest
+
 from pipescaler.processors import (ESRGANProcessor, Pixelmator2xProcessor,
                                    PotraceProcessor, ThresholdProcessor,
                                    WaifuPixelmator2xTransparentProcessor,
                                    WaifuProcessor)
 
 
+################################## FIXTURES ###################################
+@pytest.fixture(params=[ESRGANProcessor, Pixelmator2xProcessor,
+                        PotraceProcessor, ThresholdProcessor,
+                        WaifuPixelmator2xTransparentProcessor,
+                        WaifuProcessor])
+def cltool(request):
+    return request.param
+
+
 #################################### TESTS ####################################
-def test_test():
-    ESRGANProcessor.construct_argparser()
-    Pixelmator2xProcessor.construct_argparser()
-    PotraceProcessor.construct_argparser()
-    ThresholdProcessor.construct_argparser()
-    WaifuPixelmator2xTransparentProcessor.construct_argparser()
-    WaifuProcessor.construct_argparser()
+def test_construct_argparser(cltool):
+    cltool.construct_argparser()
