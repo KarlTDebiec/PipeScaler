1e51df06004e528ca5fa58ad3245723f04d7ed59
Karl Debiec <karl.t.debiec@gmail.com>
2020-08-17 20:42:44 -0500

    test test

--- test/test.py
+++ test/test.py
@@ -7,10 +7,17 @@
 #   This software may be modified and distributed under the terms of the
 #   BSD license. See the LICENSE file for details.
 ################################### MODULES ###################################
-from pipescaler.processors import WaifuProcessor
+from pipescaler.processors import (ESRGANProcessor, Pixelmator2xProcessor,
+                                   PotraceProcessor, ThresholdProcessor,
+                                   WaifuPixelmator2xTransparentProcessor,
+                                   WaifuProcessor)
 
 
 #################################### TESTS ####################################
-
 def test_test():
+    ESRGANProcessor.construct_argparser()
+    Pixelmator2xProcessor.construct_argparser()
+    PotraceProcessor.construct_argparser()
+    ThresholdProcessor.construct_argparser()
+    WaifuPixelmator2xTransparentProcessor.construct_argparser()
     WaifuProcessor.construct_argparser()
