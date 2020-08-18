c4480cb3c4b443c40a4cbf701825a2f0ed489724
Karl Debiec <karl.t.debiec@gmail.com>
2020-08-17 20:03:17 -0500

    test test

--- test/test.py
+++ test/test.py
@@ -1,6 +1,16 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+#   test.py
+#
+#   Copyright (C) 2020 Karl T Debiec
+#   All rights reserved.
+#
+#   This software may be modified and distributed under the terms of the
+#   BSD license. See the LICENSE file for details.
+################################### MODULES ###################################
+from pipescaler.processors import WaifuProcessor
 
-import pipescaler
+
+#################################### TESTS ####################################
 
 def test_test():
-    pass
+    WaifuProcessor.construct_argparser()
