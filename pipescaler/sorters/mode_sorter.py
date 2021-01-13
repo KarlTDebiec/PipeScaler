78c3ee995f6c228795d9a890935f3cc80caa6f37
Karl Debiec <karl.t.debiec@gmail.com>
2021-01-12 20:28:41 -0500

    ModeSorter may now drop alpha

--- pipescaler/sorters/mode_sorter.py
+++ pipescaler/sorters/mode_sorter.py
@@ -9,8 +9,13 @@
 ####################################### MODULES ########################################
 from __future__ import annotations
 
+from os.path import isfile
 from typing import Any, Generator, List, Optional, Union
 
+import numpy as np
+from PIL import Image
+
+from pipescaler.common import validate_int, validate_output_path
 from pipescaler.core import PipeImage, Sorter
 
 
@@ -20,15 +25,22 @@ class ModeSorter(Sorter):
     # region Builtins
 
     def __init__(
-        self,
-        downstream_stages_for_rgba: Optional[Union[str, List[str]]] = None,
-        downstream_stages_for_rgb: Optional[Union[str, List[str]]] = None,
-        downstream_stages_for_l: Optional[Union[str, List[str]]] = None,
-        **kwargs: Any,
+            self,
+            drop_alpha_threshold: Optional[int] = None,
+            downstream_stages_for_rgba: Optional[Union[str, List[str]]] = None,
+            downstream_stages_for_rgb: Optional[Union[str, List[str]]] = None,
+            downstream_stages_for_l: Optional[Union[str, List[str]]] = None,
+            **kwargs: Any,
     ) -> None:
         super().__init__(**kwargs)
 
         # Store configuration
+        if drop_alpha_threshold is None:
+            self.drop_alpha_threshold = None
+        else:
+            self.drop_alpha_threshold = validate_int(
+                drop_alpha_threshold, min_value=0, max_value=255
+            )
         if isinstance(downstream_stages_for_rgba, str):
             downstream_stages_for_rgba = [downstream_stages_for_rgba]
         self.downstream_stages_for_rgba = downstream_stages_for_rgba
@@ -46,25 +58,25 @@ class ModeSorter(Sorter):
             if len(self.downstream_stages_for_rgba) >= 2:
                 for stage in self.downstream_stages_for_rgba[:-1]:
                     desc += f"\n │   ├─ {stage}"
-            desc += f"\n │   └─ {self.downstream_stages_for_rgba[-1]}"
+            desc += f"\n │  └─ {self.downstream_stages_for_rgba[-1]}"
         else:
-            desc += f"\n │   └─"
+            desc += f"\n │  └─"
         desc += f"\n ├─ RGB"
         if self.downstream_stages_for_rgb is not None:
             if len(self.downstream_stages_for_rgb) >= 2:
                 for stage in self.downstream_stages_for_rgb[:-1]:
                     desc += f"\n │   ├─ {stage}"
-            desc += f"\n │   └─ {self.downstream_stages_for_rgb[-1]}"
+            desc += f"\n │  └─ {self.downstream_stages_for_rgb[-1]}"
         else:
-            desc += f"\n │   └─"
+            desc += f"\n │  └─"
         desc += f"\n └─ L"
         if self.downstream_stages_for_l is not None:
             if len(self.downstream_stages_for_l) >= 2:
                 for stage in self.downstream_stages_for_l[:-1]:
                     desc += f"\n     ├─ {stage}"
-            desc += f"\n     └─ {self.downstream_stages_for_l[-1]}"
+            desc += f"\n    └─ {self.downstream_stages_for_l[-1]}"
         else:
-            desc += f"\n     └─"
+            desc += f"\n    └─"
         self.desc = desc
 
     def __call__(self) -> Generator[PipeImage, PipeImage, None]:
@@ -73,9 +85,28 @@ class ModeSorter(Sorter):
             if self.pipeline.verbosity >= 2:
                 print(f"{self} sorting: {image.name} ({image.mode})")
             if image.mode == "RGBA":
-                if self.downstream_stages_for_rgba is not None:
-                    for stage in self.downstream_stages_for_rgba:
-                        self.pipeline.stages[stage].send(image)
+                if self.drop_alpha_threshold is not None and np.all(
+                        np.array(Image.open(image.last))[:, :, 3]
+                        >= self.drop_alpha_threshold
+                ):
+                    outfile = validate_output_path(
+                        self.pipeline.get_outfile(image, "RGB")
+                    )
+                    if not isfile(outfile):
+                        if self.pipeline.verbosity >= 2:
+                            print(f"{self} dropping alpha: {image.name}")
+                        rgba_image = Image.open(image.last)
+                        rgba_datum = np.array(rgba_image)
+                        rgb_image = Image.fromarray(rgba_datum[:, :, :3])
+                        rgb_image.save(outfile)
+                    image.log(self.name, outfile)
+                    if self.downstream_stages_for_rgb is not None:
+                        for stage in self.downstream_stages_for_rgb:
+                            self.pipeline.stages[stage].send(image)
+                else:
+                    if self.downstream_stages_for_rgba is not None:
+                        for stage in self.downstream_stages_for_rgba:
+                            self.pipeline.stages[stage].send(image)
             elif image.mode == "RGB":
                 if self.downstream_stages_for_rgb is not None:
                     for stage in self.downstream_stages_for_rgb:
