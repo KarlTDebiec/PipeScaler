a25614b9e32b22d5b8bee6ef8b08c5db513c3627
Karl Debiec <karl.t.debiec@gmail.com>
2021-01-16 15:13:37 -0500

    copyright updated

--- pipescaler/scripts/pipe_runner.py
+++ pipescaler/scripts/pipe_runner.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #   pipescaler/scripts/pipe_runner.py
 #
-#   Copyright (C) 2020 Karl T Debiec
+#   Copyright (C) 2020-2021 Karl T Debiec
 #   All rights reserved.
 #
 #   This software may be modified and distributed under the terms of the
