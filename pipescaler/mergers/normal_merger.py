425722d31b5f8c9ba1f9827eea685fd3d37d0c64
Karl Debiec <karl.t.debiec@gmail.com>
2021-05-05 20:43:47 -0400

    fixed normal map merger bug

--- pipescaler/mergers/normal_merger.py
+++ pipescaler/mergers/normal_merger.py
@@ -61,16 +61,15 @@ class NormalMerger(Merger):
             if not isfile(outfile):
                 if self.pipeline.verbosity >= 2:
                     print(f"    merging: {image.name}")
-                # r_datum = np.array(Image.open(r_infile).convert("L"))
-                # g_datum = np.array(Image.open(g_infile).convert("L"))
-                # b_datum = np.array(Image.open(b_infile).convert("L"))
                 r_datum = np.array(Image.open(r_infile).convert("L"), np.float) - 128
                 g_datum = np.array(Image.open(g_infile).convert("L"), np.float) - 128
                 b_datum = np.array(Image.open(b_infile).convert("L"), np.float) - 128
+                b_datum[b_datum < 0] = 0
                 mag = np.sqrt(r_datum ** 2 + g_datum ** 2 + b_datum ** 2)
                 r_datum = (((r_datum / mag) * 128) + 128).astype(np.uint8)
                 g_datum = (((g_datum / mag) * 128) + 128).astype(np.uint8)
                 b_datum = (((b_datum / mag) * 128) + 128).astype(np.uint8)
+                b_datum[b_datum == 0] = 255
                 rgb_datum = np.zeros((r_datum.shape[0], r_datum.shape[1], 3), np.uint8)
                 rgb_datum[:, :, 0] = r_datum
                 rgb_datum[:, :, 1] = g_datum
