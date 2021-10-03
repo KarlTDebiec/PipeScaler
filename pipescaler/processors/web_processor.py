591bfa4f67bb96765e12a54077ecc81ebab0f73e
Karl T Debiec <karl.t.debiec@gmail.com>
2021-10-03 16:52:06 -0400

    updated README

--- pipescaler/processors/web_processor.py
+++ pipescaler/processors/web_processor.py
@@ -20,7 +20,7 @@ from pipescaler.core import Processor
 
 
 class WebProcessor(Processor):
-    """POSTs image to a defined URL, which response with a processed image."""
+    """POSTs image to a defined URL, which responds with processed image."""
 
     def __init__(self, url: str, **kwargs: Any) -> None:
         super().__init__(**kwargs)
