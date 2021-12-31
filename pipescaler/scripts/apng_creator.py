a407b263240d7806de0de852019a629ce5212d6d
Karl T Debiec <karl.t.debiec@gmail.com>
2021-12-31 12:36:04 -0500

    wip

--- pipescaler/scripts/apng_creator.py
+++ pipescaler/scripts/apng_creator.py
@@ -136,7 +136,9 @@ class ApngCreator(CLTool):
             help="labels with which to annotate images",
         )
         parser.add_argument(
-            "--show_size", action="store_true", help="annotate each image with size",
+            "--show_size",
+            action="store_true",
+            help="annotate each image with size",
         )
         parser.add_argument(
             "--duration",
