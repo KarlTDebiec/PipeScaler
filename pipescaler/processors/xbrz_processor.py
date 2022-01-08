850c18e437789873326ee38214e435a175851c22
Karl T Debiec <karl.t.debiec@gmail.com>
2022-01-08 10:56:39 -0500

    Update copyright (#37)

--- pipescaler/processors/xbrz_processor.py
+++ pipescaler/processors/xbrz_processor.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #   pipescaler/processors/xbrz_processor.py
 #
-#   Copyright (C) 2020-2021 Karl T Debiec
+#   Copyright (C) 2020-2022 Karl T Debiec
 #   All rights reserved.
 #
 #   This software may be modified and distributed under the terms of the
