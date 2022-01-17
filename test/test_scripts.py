2deae8628457118ab9c1799e34e6fffc97557630
Karl T Debiec <karl.t.debiec@gmail.com>
2022-01-17 10:29:17 -0500

    wip

--- test/test_scripts.py
+++ test/test_scripts.py
@@ -16,7 +16,7 @@ scripts = {
     f: join(package_root, "scripts", f)
     for f in [
         "apng_creator.py",
-        "directory_watcher.py",
+        "file_watcher.py",
         "pipe_runner.py",
         "pipescaler_host.py",
         "scaled_image_identifier.py",
