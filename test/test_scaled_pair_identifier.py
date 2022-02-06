596fa7eec384d0cd0c328076e0b94946266bb9de
Karl T Debiec <karl.t.debiec@gmail.com>
2022-02-06 14:10:14 -0500

    wip

--- test/test_scaled_pair_identifier.py
+++ test/test_scaled_pair_identifier.py
@@ -7,10 +7,12 @@
 #   This software may be modified and distributed under the terms of the
 #   BSD license. See the LICENSE file for details.
 from os.path import basename, join, splitext
+from platform import system
 from shutil import copy
 from tempfile import TemporaryDirectory
 
 import numpy as np
+import pytest
 from PIL import Image
 from shared import alt_infiles, infiles
 
@@ -19,13 +21,16 @@ from pipescaler.core import get_files
 from pipescaler.util import ScaledPairIdentifier
 
 
+@pytest.mark.xfail(
+    system() in {"Linux"}, raises=OSError, reason=f"Not supported on {system()}"
+)
 def test_review() -> None:
     with TemporaryDirectory() as input_directory:
         for mode in ["L", "LA", "RGB", "RGBA"]:
             base_filename = splitext(basename(infiles[mode]))[0]
             copy(infiles[mode], join(input_directory, f"{base_filename}_1.png"))
             parent = Image.open(infiles[mode])
-            for scale in np.array([1 / (2 ** x) for x in range(1, 7)]):
+            for scale in np.array([1 / (2**x) for x in range(1, 7)]):
                 width = round(parent.size[0] * scale)
                 height = round(parent.size[1] * scale)
                 if width < 8 or height < 8:
@@ -40,7 +45,7 @@ def test_review() -> None:
             base_filename = splitext(basename(alt_infiles[mode]))[0]
             copy(alt_infiles[mode], join(input_directory, f"{base_filename}_alt_1.png"))
             parent = Image.open(alt_infiles[mode])
-            for scale in np.array([1 / (2 ** x) for x in range(1, 7)]):
+            for scale in np.array([1 / (2**x) for x in range(1, 7)]):
                 width = round(parent.size[0] * scale)
                 height = round(parent.size[1] * scale)
                 if width < 8 or height < 8:
