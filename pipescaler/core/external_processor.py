f64180a962ee96602f962e2ac2a853e1f412f328
Karl Debiec <karl.t.debiec@gmail.com>
2022-02-27 12:40:57 -0500

    wip

--- pipescaler/core/external_processor.py
+++ pipescaler/core/external_processor.py
@@ -67,7 +67,7 @@ class ExternalProcessor(Processor, ABC):
         """
         command = self.command_template.format(infile=infile, outfile=outfile)
         debug(f"{self}: {command}")
-        run_command(command)
+        run_command(command, timeout=10)
 
         # If command template lacks outfile, assume infile is processed in place
         if "{outfile}" not in self.command_template:
