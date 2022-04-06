6cda504bb6f8c7ccc77b68b77fd7f9420f72ddd2
Karl T Debiec <karl.t.debiec@gmail.com>
2022-04-05 22:48:26 -0400

    wip

--- pipescaler/processors/__init__.py
+++ pipescaler/processors/__init__.py
@@ -5,10 +5,50 @@
 """Processor stages."""
 from __future__ import annotations
 
+from pipescaler.processors.external import (
+    AppleScriptProcessor,
+    AutomatorProcessor,
+    PngquantProcessor,
+    PotraceProcessor,
+    TexconvProcessor,
+    WaifuExternalProcessor,
+)
+from pipescaler.processors.gui import GigapixelAiProcessor
+from pipescaler.processors.image import (
+    CropProcessor,
+    EsrganProcessor,
+    ExpandProcessor,
+    HeightToNormalProcessor,
+    ModeProcessor,
+    ResizeProcessor,
+    SharpenProcessor,
+    SolidColorProcessor,
+    ThresholdProcessor,
+    WaifuProcessor,
+    XbrzProcessor,
+)
 from pipescaler.processors.side_channel_processor import SideChannelProcessor
 from pipescaler.processors.web_processor import WebProcessor
 
 __all__: list[str] = [
+    "AppleScriptProcessor",
+    "AutomatorProcessor",
+    "CropProcessor",
+    "EsrganProcessor",
+    "ExpandProcessor",
+    "GigapixelAiProcessor",
+    "HeightToNormalProcessor",
+    "ModeProcessor",
+    "PngquantProcessor",
+    "PotraceProcessor",
+    "ResizeProcessor",
+    "SharpenProcessor",
     "SideChannelProcessor",
+    "SolidColorProcessor",
+    "TexconvProcessor",
+    "ThresholdProcessor",
+    "WaifuExternalProcessor",
+    "WaifuProcessor",
     "WebProcessor",
+    "XbrzProcessor",
 ]
