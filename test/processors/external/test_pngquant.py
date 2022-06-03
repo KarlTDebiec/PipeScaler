ca595fcf6d99343a92b266fe5d56fae3123e1d32
Karl T Debiec <karl.t.debiec@gmail.com>
2022-06-03 08:34:54 -0400

    wip

--- test/processors/external/test_pngquant.py
+++ test/processors/external/test_pngquant.py
@@ -26,15 +26,15 @@ def processor(request) -> PngquantProcessor:
 @pytest.mark.parametrize(
     ("infile"),
     [
-        xfail_if_platform({"Windows", ExecutableNotFoundError})("1"),
-        xfail_if_platform({"Windows", ExecutableNotFoundError})("L"),
-        xfail_if_platform({"Windows", ExecutableNotFoundError})("LA"),
-        xfail_if_platform({"Windows", ExecutableNotFoundError})("RGB"),
-        xfail_if_platform({"Windows", ExecutableNotFoundError})("RGBA"),
-        xfail_if_platform({"Windows", ExecutableNotFoundError})("PL"),
-        xfail_if_platform({"Windows", ExecutableNotFoundError})("PLA"),
-        xfail_if_platform({"Windows", ExecutableNotFoundError})("PRGB"),
-        xfail_if_platform({"Windows", ExecutableNotFoundError})("PRGBA"),
+        xfail_if_platform({"Windows"}, ExecutableNotFoundError)("1"),
+        xfail_if_platform({"Windows"}, ExecutableNotFoundError)("L"),
+        xfail_if_platform({"Windows"}, ExecutableNotFoundError)("LA"),
+        xfail_if_platform({"Windows"}, ExecutableNotFoundError)("RGB"),
+        xfail_if_platform({"Windows"}, ExecutableNotFoundError)("RGBA"),
+        xfail_if_platform({"Windows"}, ExecutableNotFoundError)("PL"),
+        xfail_if_platform({"Windows"}, ExecutableNotFoundError)("PLA"),
+        xfail_if_platform({"Windows"}, ExecutableNotFoundError)("PRGB"),
+        xfail_if_platform({"Windows"}, ExecutableNotFoundError)("PRGBA"),
     ],
 )
 def test(infile: str, processor: PngquantProcessor) -> None:
