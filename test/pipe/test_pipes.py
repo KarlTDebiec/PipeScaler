0b7f6a8726f87891a3c785bdc80a8ecccb740b54
Karl T Debiec <karl.t.debiec@gmail.com>
2022-06-04 09:38:09 -0400

    wip

--- test/pipe/test_pipes.py
+++ test/pipe/test_pipes.py
@@ -2,30 +2,36 @@
 #   Copyright (C) 2020-2022 Karl T Debiec
 #   All rights reserved. This software may be modified and distributed under
 #   the terms of the BSD license. See the LICENSE file for details.
-"""Tests for AlphaSplitterPipe."""
+"""Tests for Pipes."""
 from __future__ import annotations
 
 from PIL import Image
 from pytest import mark
 
 from pipescaler.core import PipeImage
-from pipescaler.core.pipe import SplitterPipe
+from pipescaler.core.pipe.pipe import Pipe
+from pipescaler.pipe.mergers.alpha_merger_pipe import AlphaMergerPipe
+from pipescaler.pipe.processors import XbrzProcessorPipe
 from pipescaler.pipe.splitters.alpha_splitter_pipe import AlphaSplitterPipe
 from pipescaler.testing import get_infile
 
 
 @mark.parametrize(
-    ("pipe", "infile"),
+    ("pipe", "infiles"),
     [
-        (AlphaSplitterPipe(), "RGBA"),
+        (AlphaMergerPipe(), ("split/RGBA_color_RGB", "split/RGBA_alpha_L")),
+        (AlphaSplitterPipe(), ("RGBA",)),
+        (XbrzProcessorPipe(), ("RGB",)),
     ],
 )
-def test(pipe: SplitterPipe, infile: str) -> None:
-    inlet = PipeImage(Image.open(get_infile(infile)))
-    inlets = {"inlet": inlet}
+def test(pipe: Pipe, infiles: tuple[str]) -> None:
+    inlets = {
+        pipe.inlets[i]: PipeImage(Image.open(get_infile(infile)))
+        for i, infile in enumerate(infiles)
+    }
     assert list(inlets.keys()) == pipe.inlets
 
     outlets = pipe(inlets)
     assert list(outlets.keys()) == pipe.outlets
     for outlet in outlets:
-        assert outlets[outlet].parent == inlet
+        assert outlets[outlet].parent == list(inlets.values())
