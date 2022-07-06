eacbe0a167d0de7c4e6def8b00d4382cfac28470
Karl T Debiec <karl.t.debiec@gmail.com>
2022-07-05 23:04:40 -0400

    wip

--- pipescaler/image/processors/esrgan_processor.py
+++ pipescaler/image/processors/esrgan_processor.py
@@ -1,14 +1,7 @@
 #!/usr/bin/env python
-
 #  Copyright (C) 2020-2022. Karl T Debiec
 #  All rights reserved. This software may be modified and distributed under
 #  the terms of the BSD license. See the LICENSE file for details.
-
-#   pipescaler/processors/esrgan_processor.py
-#
-#
-#   This software may be modified and distributed under the terms of the
-#   BSD license.
 """Upscales and/or denoises image using ESRGAN."""
 from __future__ import annotations
 
