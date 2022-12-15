d810b4e388875a1a981f4fe0391d06d6456ad992
Karl T Debiec <karl.t.debiec@gmail.com>
2022-12-15 00:08:00 -0500

    HierarchicalDirectorySource first pass complete

--- pipescaler/pipelines/sources/hierarchical_directory_source.py
+++ pipescaler/pipelines/sources/hierarchical_directory_source.py
@@ -5,10 +5,11 @@
 """Yields images from a directory."""
 from __future__ import annotations
 
+import re
 from pathlib import Path
-from typing import Any, Callable, Optional, Sequence, Union
+from typing import Any, Callable, Optional, Union
 
-from pipescaler.common import validate_input_directories
+from pipescaler.common import validate_input_directory
 from pipescaler.core.pipelines import PipeImage, Source
 from pipescaler.core.sorting import basic_sort
 
@@ -16,12 +17,12 @@ from pipescaler.core.sorting import basic_sort
 class HierarchicalDirectorySource(Source):
     """Yields images from a directory."""
 
-    cls_exclusions = {".DS_Store", "desktop"}
-    """Base filenames to exclude"""
+    cls_exclusions = {r".*\.DS_Store$", r".*Thumbs.db$", r".*desktop$"}
+    """File paths to exclude"""
 
     def __init__(
         self,
-        directory: Union[Union[Path, str], Sequence[Union[Path, str]]],
+        directory: Union[Path, str],
         exclusions: Optional[set[str]] = None,
         sort: Union[Callable[[str], int], Callable[[str], str]] = basic_sort,
         reverse: bool = False,
@@ -30,8 +31,8 @@ class HierarchicalDirectorySource(Source):
         """Validate and store configuration and initialize.
 
         Arguments:
-            directory: Directory or directories from which to yield files
-            exclusions: Filenames stems to exclude
+            directory: Directory from which to yield files
+            exclusions: File path to exclude
             sort: Function to sort filenames
             reverse: Whether to reverse sort order
             **kwargs: Additional keyword arguments
@@ -39,31 +40,41 @@ class HierarchicalDirectorySource(Source):
         super().__init__(**kwargs)
 
         # Store configuration
-        self.directories = validate_input_directories(directory)
-        self.exclusions = self.cls_exclusions
-        if exclusions is not None:
-            self.exclusions |= exclusions
+        self.directory = validate_input_directory(directory)
+        if exclusions is None:
+            exclusions = set()
+        exclusions |= self.cls_exclusions
+        self.exclusions = set()
+        for exclusion in exclusions:
+            if isinstance(exclusion, str):
+                exclusion = re.compile(exclusion)
+            elif not isinstance(exclusion, re.Pattern):
+                raise TypeError(
+                    f"Exclusion must be str or re.Pattern, not {type(exclusion)}"
+                )
+            self.exclusions.add(exclusion)
         self.sort = sort
         self.reverse = reverse
 
         # Store list of file_paths
-        file_paths = []
-        for directory in self.directories:
-            file_paths.extend(self.scandir(directory))
+        file_paths = self.scandir(self.directory, self.directory)
         file_paths.sort(
-            key=lambda file_path: self.sort(file_path), reverse=self.reverse
+            key=lambda file_path: self.sort(str(file_path.relative_to(self.directory))),
+            reverse=self.reverse,
         )
         self.file_paths = file_paths
         self.index = 0
 
-    def scandir(self, directory: Path) -> list[Path]:
+    def scandir(self, root_directory: Path, directory: Path) -> list[Path]:
         """Yield next image."""
         file_paths = []
         for file_path in [f for f in directory.iterdir() if f.is_file()]:
-            if file_path.stem not in self.exclusions:
-                file_paths.append(file_path)
+            relative_path = file_path.relative_to(root_directory)
+            if any(er.match(str(relative_path)) for er in self.exclusions):
+                continue
+            file_paths.append(file_path)
         for subdirectory_path in [d for d in directory.iterdir() if d.is_dir()]:
-            file_paths.extend(self.scandir(subdirectory_path))
+            file_paths.extend(self.scandir(root_directory, subdirectory_path))
 
         return file_paths
 
