7893a2bf7283b5dff891e1389d64bc82ecf884b4
Karl T Debiec <karl.t.debiec@gmail.com>
2022-12-16 20:46:15 -0500

    Doc updates (#354)

--- pipescaler/pipelines/sorters/solid_color_sorter.py
+++ pipescaler/pipelines/sorters/solid_color_sorter.py
@@ -15,7 +15,7 @@ from pipescaler.core.pipelines import PipeImage, Sorter
 
 
 class SolidColorSorter(Sorter):
-    """Sorts image based on whether their entire canvas is a solid color."""
+    """Sorts images based on whether their entire canvas is a solid color."""
 
     def __init__(self, mean_threshold: int = 1, max_threshold: int = 10) -> None:
         """Validate configuration and initialize.
