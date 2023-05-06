4cdeb6ce34afd951fce15e1ef338f7caf5f58714
Karl T Debiec <karl.t.debiec@gmail.com>
2023-05-06 12:51:29 -0400

    wip

--- pipescaler/image/core/pipelines/image_operator_segment.py
+++ pipescaler/image/core/pipelines/image_operator_segment.py
@@ -4,10 +4,11 @@
 """Abstract base class for segments that apply ImageOperators."""
 from __future__ import annotations
 
-from abc import ABC
+from abc import ABC, abstractmethod
 
 from pipescaler.image.core import ImageOperator
 from pipescaler.image.core.pipelines.image_segment import ImageSegment
+from pipescaler.image.core.pipelines.pipe_image import PipeImage
 
 
 class ImageOperatorSegment(ImageSegment, ABC):
@@ -23,6 +24,17 @@ class ImageOperatorSegment(ImageSegment, ABC):
         """
         self.operator = operator
         """Operator to apply"""
+
+    @abstractmethod
+    def __call__(self, *inputs: PipeImage) -> tuple[PipeImage, ...]:
+        """Process an image.
+
+        Arguments:
+            inputs: Input image, within a tuple for consistency with other Segments
+        Returns:
+            Output image, within a tuple for consistency with other Segments
+        """
+        raise NotImplementedError()
 
     def __repr__(self) -> str:
         """Representation."""
