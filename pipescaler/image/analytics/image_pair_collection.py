0d3576bad4037f0fd91c55493518ae1927c7df5d
Karl T Debiec <karl.t.debiec@gmail.com>
2023-05-09 20:16:26 -0400

    Testing GitHub Actions

--- pipescaler/image/analytics/image_pair_collection.py
+++ pipescaler/image/analytics/image_pair_collection.py
@@ -2,7 +2,7 @@
 #  Copyright 2020-2023 Karl T Debiec
 #  All rights reserved. This software may be modified and distributed under
 #  the terms of the BSD license. See the LICENSE file for details.
-"""Collection of image pairs."""
+"""Collection of image pairs _."""
 from __future__ import annotations
 
 from collections.abc import Sequence
