90353ba24aaa2e1202835d00a5551b0abc14e87c
Karl T Debiec <karl.t.debiec@gmail.com>
2023-05-13 13:05:06 -0400

    Shrink four-line header to two lines

--- pipescaler/image/scaled_pair_identifier.py
+++ pipescaler/image/scaled_pair_identifier.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 #  Copyright 2020-2023 Karl T Debiec. All rights reserved. This software may be modified
 #  and distributed under the terms of the BSD license. See the LICENSE file for details.
 """Identifies pairs of images in which one is rescaled from another."""
