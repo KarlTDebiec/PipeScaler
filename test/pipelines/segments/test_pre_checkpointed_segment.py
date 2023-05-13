977680808fab9c641703e0a7d168207083d7aa2c
Karl T Debiec <karl.t.debiec@gmail.com>
2023-05-13 13:10:41 -0400

    Reduce four-line header to two lines (#458)

--- test/pipelines/segments/test_pre_checkpointed_segment.py
+++ test/pipelines/segments/test_pre_checkpointed_segment.py
@@ -1,6 +1,5 @@
-#  Copyright 2020-2023 Karl T Debiec
-#  All rights reserved. This software may be modified and distributed under
-#  the terms of the BSD license. See the LICENSE file for details.
+#  Copyright 2020-2023 Karl T Debiec. All rights reserved. This software may be modified
+#  and distributed under the terms of the BSD license. See the LICENSE file for details.
 """Tests for PreCheckpointedSegment."""
 from __future__ import annotations
 
