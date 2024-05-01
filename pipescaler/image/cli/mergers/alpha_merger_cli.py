5d1e7fae0638c2215fb028a6fcfefbed0d0a7f79
Karl Debiec <karl.t.debiec@gmail.com>
2024-04-30 21:01:03 -0400

    Address grievance

--- pipescaler/image/cli/mergers/alpha_merger_cli.py
+++ pipescaler/image/cli/mergers/alpha_merger_cli.py
@@ -2,7 +2,7 @@
 #  Copyright 2020-2024 Karl T Debiec. All rights reserved. This software may be modified
 #  and distributed under the terms of the BSD license. See the LICENSE file for details.
 """Command-line interface for AlphaMerger."""
-from __future__ import annotations, annotations
+from __future__ import annotations
 
 from argparse import ArgumentParser
 from logging import info
