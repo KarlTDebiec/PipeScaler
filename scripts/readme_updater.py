62dbcff9f8d9c4bc05014483c9ff769faf41cb0f
Karl T Debiec <karl.t.debiec@gmail.com>
2024-05-04 09:19:56 -0400

    Update copyright to 2024 (#623)

--- scripts/readme_updater.py
+++ scripts/readme_updater.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#  Copyright 2020-2023 Karl T Debiec. All rights reserved. This software may be modified
+#  Copyright 2020-2024 Karl T Debiec. All rights reserved. This software may be modified
 #  and distributed under the terms of the BSD license. See the LICENSE file for details.
 """Updates README."""
 from __future__ import annotations
