4ed3987a547afe7885154cdad4ac4736d1ac60c2
Karl T Debiec <karl.t.debiec@gmail.com>
2025-07-16 18:30:50 -0400

    Update common and fix docstrings (#729)

--- pipescaler/image/subdivided_image.py
+++ pipescaler/image/subdivided_image.py
@@ -1,4 +1,4 @@
-#  Copyright 2020-2024 Karl T Debiec. All rights reserved. This software may be modified
+#  Copyright 2020-2025 Karl T Debiec. All rights reserved. This software may be modified
 #  and distributed under the terms of the BSD license. See the LICENSE file for details.
 """Image divided into subdivisions."""
 from __future__ import annotations
@@ -174,7 +174,6 @@ class SubdividedImage:
         Arguments:
             image: Image to be subdivided
             boxes: Boxes for subdivisions in form of [[left, upper, right, lower], ...]
-
         Returns:
             Subdivisions of image
         """
