d2ccfb9ca9b42b7caec54927ea094257f9093570
Karl Debiec <karl.t.debiec@gmail.com>
2026-05-03 15:18:32 -0400

    Add Spandrel processor CLI

--- pipescaler/image/cli/processors/__init__.py
+++ pipescaler/image/cli/processors/__init__.py
@@ -2,10 +2,12 @@
 #  and distributed under the terms of the BSD license. See the LICENSE file for details.
 """PipeScaler image processor command-line interfaces package.
 
-This module may import from: common, core, image.core, image.core.cli, image.operators.processors
+This module may import from: common, core, image.core, image.core.cli,
+image.operators.processors
 
 Hierarchy within module:
-* crop_cli / expand_cli / mode_cli / resize_cli / solid_color_cli / threshold_cli / height_to_normal_cli / sharpen_cli / xbrz_cli
+* crop_cli / expand_cli / mode_cli / resize_cli / solid_color_cli / threshold_cli
+* height_to_normal_cli / sharpen_cli / spandrel_cli / xbrz_cli
 """
 
 from __future__ import annotations
@@ -17,6 +19,7 @@ from .mode_cli import ModeCli
 from .resize_cli import ResizeCli
 from .sharpen_cli import SharpenCli
 from .solid_color_cli import SolidColorCli
+from .spandrel_cli import SpandrelCli
 from .threshold_cli import ThresholdCli
 from .xbrz_cli import XbrzCli
 
@@ -28,6 +31,7 @@ __all__ = [
     "ResizeCli",
     "SharpenCli",
     "SolidColorCli",
+    "SpandrelCli",
     "ThresholdCli",
     "XbrzCli",
 ]
