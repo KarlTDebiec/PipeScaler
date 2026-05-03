c0f00fabaa27f6f3cfd4db92a64cfe81b3240169
Karl Debiec <karl.t.debiec@gmail.com>
2026-05-03 15:09:42 -0400

    Remove scaled pair identifier

--- pipescaler/image/core/analytics/__init__.py
+++
@@ -1,8 +0,0 @@
-#  Copyright 2020-2026 Karl T Debiec. All rights reserved. This software may be modified
-#  and distributed under the terms of the BSD license. See the LICENSE file for details.
-"""PipeScaler image core analytics package.
-
-This module may import from: common, core, image.core
-"""
-
-from __future__ import annotations
